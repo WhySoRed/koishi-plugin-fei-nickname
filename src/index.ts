b4a51dc7a2667167dfcf7cd243e5f8efc34b8a84
WhySoRed <yousefmazbah57@gmail.com>
2024-04-28 01:57:46 +0800

    modified:   src/index.ts

--- src/index.ts
+++ src/index.ts
@@ -9,16 +9,59 @@ export const inject = {
 export const name = 'fei-nickname'
 
 export interface Config {
-    isQQofficalBot:boolean
     defaultNickName:string
     globalEnableDoSomeThing:boolean
 }
 
 export const Config: Schema<Config> = Schema.object({
-    isQQofficalBot: Schema.boolean().default(false).description('是否是qq官方机器人(无法获取用户名)'),
     defaultNickName: Schema.string().default('那个谁').description('获取用户名失败时的默认名称'),
     globalEnableDoSomeThing: Schema.boolean().default(true).description('开启动手动脚功能'),
 })
+
+export const nickName = {
+    //根据输入的session返回session的发送者的自称
+    //没有自称则会以群昵称>平台昵称>默认昵称的优先级向后获取
+    getSelfName : async (session:any) => {
+        const ctx = session.app;
+        const platform = session.platform;
+        const userId = session.event.user.id;
+        const userData = (await ctx.database.get('nickNameUserData',{ platform, userId }))[0]
+        if(userData.nameBeSet)
+            return userData.nickName;
+        else if(session.event.member.nick)
+            return session.event.member.nick;
+        else if(session.event.user.name)
+            return session.event.user.name;
+        else
+            return userData.nickName;
+    },
+    //根据输入的session和text，获取text中包含的<at id="(平台id)">的id
+    //返回一个这些id转换为名字的数组
+    //优先级是session发送者给对方的外号>自称>群昵称>平台昵称>默认昵称
+    getNickNameGivenFromText : async (session: any, text: string) => {
+        const ctx = session.app;
+        const match = Array.from(text.matchAll(/<at id="(\d+)"\/>/g));
+        if (match.length === 0) return [];
+        const platform = session.platform;
+        return match.map((element) => element[1]).map(async (userId) => {
+            const callerUserId = session.event.user.id;
+            const receiverUserId = userId;
+            const nickNameGiven = (await ctx.database.get('nickNameGivenData',{ platform, callerUserId, receiverUserId}))[0]?.nickNameGiven;
+            if(nickNameGiven)
+                return nickNameGiven;
+            else {
+                const userId = receiverUserId;
+                const userData = (await ctx.database.get('nickNameUserData',{ platform, userId }))[0]
+                if(!session.event.channel.type) {
+                    const member = await session.bot.getGuildMember(session.guildId, userId);
+                    if(member)
+                        return member.nick? member.nick: member.user.name;
+                }
+                return userData.nickName;
+            }
+        })
+    }
+}
 
 declare module 'koishi' {
     interface Tables {
@@ -35,14 +78,15 @@ export interface NickNameUserData {
     nameBeSet: boolean
     enableNickNamed: boolean
     enableDoSomeThing: boolean
-    enableNickNamedBlacklist: string[]
-    enableDoSomeThingBlacklist: string[]
+    nickNamedBlacklist: string[]
+    doSomeThingBlacklist: string[]
 }
 
 export interface NickNameGivenData {
-    callerUserId: number
-    receiverUserId: number
-    nickName: string
+    platform: string
+    callerUserId: string
+    receiverUserId: string
+    nickNameGiven: string
 }
 
 export function apply(ctx: Context, config: Config) {
@@ -60,46 +104,46 @@ export function apply(ctx: Context, config: Config) {
     },{
         primary: 'id',
         autoInc: true,
-        unique: [['platform','userId']]
+        unique: [['platform','userId']]     // 用键值对的唯一性保证单一平台的单一用户的自称
     })
 
-    //这个数据表用于储存caller对receiver起了nickName这个外号这一关系
+    // 该数据表用于储存caller对receiver的nickName关系
     ctx.model.extend('nickNameGivenData', {
-        nickNameGiven: {type :'string'},
-        callerUserId: 'unsigned',
-        receiverUserId: 'unsigned',
+        platform: 'string',
+        callerUserId: 'string',
+        receiverUserId: 'string',
+        nickNameGiven: 'string',
     },{
-        primary: ['callerUserId','receiverUserId'],           //※ 用[Aid,Bid]作为主键来确保这一关系的唯一性 ※
-        foreign: {'callerUserId': ['nickNameUserData', 'id'],
-                  'receiverUserId': ['nickNameUserData', 'id']}
+        primary: ['platform','callerUserId','receiverUserId'],           // ※ 确保这一关系的唯一性 ※
     })
 
-    ctx.command('外号测试',).action(async (_) => {
-        return JSON.stringify(_.session.event.platform);
+    ctx.command('外号测试').action(async (_) => {
+        console.log(_.session.user);
+        console.log(_.session.event.message.content);
     })
 
 
-    ctx.command('外号',).action((_) => {
+    ctx.command('外号').action((_) => {
         return '';
     })
 
-    ctx.command('外号.我叫',).action(() => {
+    ctx.command('外号.设定自称').action(() => {
         return '';
     })
 
-    ctx.command('外号.取消自称',).action(() => {
+    ctx.command('外号.取消自称').action(() => {
         return '';
     })
 
-    ctx.command('外号.我被别人取的外号',).action(() => {
+    ctx.command('外号.我被别人取的外号').action(({ session }) => {
         return '';
     })
 
-    ctx.command('外号.我给别人取的外号',).action(() => {
+    ctx.command('外号.我给别人取的外号').action(() => {
         return '';
     })
 
-    ctx.command('外号.给 @对方 起外号 xxx/取消外号',).action(async (_) => {
+    ctx.command('外号.给 @对方 起外号 xxx/取消外号').action(async (_) => {
         
         if( _.session.event.channel.type ) return '这个只能在群里玩哦';
         //用户输入 给 @xx 起外号 xx
@@ -125,6 +169,16 @@ export function apply(ctx: Context, config: Config) {
         else return '指令格式太不对，空格记得分开哦' 
     })
 
+    //禁止起外号并清空外号
+    ctx.command('外号.不许给我取外号').action(() => {
+
+        return '';
+    })
+    
+    ctx.command('外号.允许给我起外号').action(() => {
+        return '';
+    })
+
     ctx.command('外号.我要 <做的事情> @对方/外号',).action(async (_) => {
         if (!config.globalEnableDoSomeThing) return '本功能未开放~'
         const match = _.args[1].match(/<at id="(\d+)"\/>/)
@@ -141,20 +195,11 @@ export function apply(ctx: Context, config: Config) {
             return '没艾特到啊';
     })
 
-    //禁止起外号并清空外号
-    ctx.command('外号.不许给我取外号',).action(() => {
-
+    ctx.command('外号.不许对我动手动脚').action(() => {
         return '';
     })
-    
-    ctx.command('外号.允许给我起外号',).action(() => {
+    ctx.command('外号.允许对我动手动脚').action(() => {
         return '';
     })
 
-    ctx.command('外号.不许对我动手动脚',).action(() => {
-        return '';
-    })
-    ctx.command('外号.允许对我动手动脚',).action(() => {
-        return '';
-    })
 }
